ecf2717953f7b99923fe1ac8c51d440084cbed78
adolfo <banchioadolfo@gmail.com>
2022-06-09 17:45:52 -0300

    framebuffer secundario

--- Ejercicio2/app.s
+++ Ejercicio2/app.s
@@ -1,16 +1,60 @@
 .include "data.s"
 .include "graphics.s"
 
+
 .globl main
+crearDelay:
+        ldr x9, retardo
+    loop_crearDelay:
+        subs x9, x9, 1
+        b.ne loop_crearDelay
+        br lr
+
+actualizarFrameBuffer:
+        mov x9, SCREEN_WIDTH
+        mov x10, SCREEN_HEIGH
+    loop_actualizarFrameBuffer:
+		madd x12,x10,x25,x9 
+        ldr w11, [x26,x12,lsl 2] // copio el color de cada pixel del frame secundario
+        str w11, [x27,x12,lsl 2] // lo pego en el principal
+		sub x9,x9,1
+		cbnz x9, loop_actualizarFrameBuffer
+		//si x9 es 0, entonces vuelvo x9 a la derecha de la linea	
+		mov x9, SCREEN_WIDTH
+		sub x10,x10,1
+		cbz x10,end_loop_actualizarFrameBuffer
+        b loop_actualizarFrameBuffer
+    end_loop_actualizarFrameBuffer:
+        br lr // return
+
 main:
 	// X0 contiene la direccion base del framebuffer
-	mov x26, SCREEN_HEIGH //  Guardamos la altura de la pantalla en x26
-	mov x25, SCREEN_WIDTH // Guardamos el ancho de la pantalla en x25
- 	mov x27, x0	// Guardamos la dirección base del framebuffer en x27
+	mov x25,SCREEN_WIDTH
+	ldr x26,=buffersecundario
+ 	mov x27,x0	// Guardamos la dirección base del framebuffer en x27
 	//---------------- CODE HERE ------------------------------------
-	
+prueba:
 	bl background
 
+	ldr x22,=navea
+	ldur w1,[x22]
+	ldur w0,[x22,4]
+	mov x3,20
+	mov x2,20
+	mov x4,white
+	bl rectangle
+
+	bl actualizarFrameBuffer
+
+	ldur w1,[x22]
+	ldur w0,[x22,4]
+	//add w1,w1,1
+	add w0,w0,1
+	stur w1,[x22]
+	stur w0,[x22,4]
+	
+	bl crearDelay
+	b prueba
 //---------------------------------------------------------------
 	// Infinite Loop 
 
--- Ejercicio2/data.s
+++ Ejercicio2/data.s
@@ -6,7 +6,10 @@
 */
 
 .data
+buffersecundario: .skip BYTES_FRAMEBUFFER
 retardo: .dword 0xffffff 
+//arreglos de posiciones
+navea: .word  320,240
 // Colores
 .equ white,         0xFFFFFF
 .equ red,           0xFF0000
@@ -16,9 +19,14 @@ yellow: .word         0xFFFF00
 .equ blue,         0x001933
 
 // Pantalla 
+
 .equ SCREEN_WIDTH, 		640
 .equ SCREEN_HEIGH, 		480
 .equ BITS_PER_PIXEL,  	32
+.equ BYTES_PER_PIXEL,    4   
+.equ SCREEN_PIXELS,     SCREEN_HEIGH*SCREEN_WIDTH
+.equ BYTES_FRAMEBUFFER,  SCREEN_PIXELS * BYTES_PER_PIXEL
+
 
 .endif
 
