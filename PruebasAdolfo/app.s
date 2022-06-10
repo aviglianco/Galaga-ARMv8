df213dc25cec28e4e12680bf6cdd3b34556e82ab
adolfo <banchioadolfo@gmail.com>
2022-06-10 16:45:40 -0300

    pruebas Adolfo (nada a ej2)

---
+++ PruebasAdolfo/app.s
@@ -0,0 +1,36 @@
+.include "data.s"
+.include "graphics.s"
+.include "background.s"
+.include "logic.s"
+.include "entry.s"
+
+
+.globl main
+main:
+	// X0 contiene la direccion base del framebuffer
+	mov x25,SCREEN_WIDTH
+	ldr x26,=buffer_secundary
+ 	mov x27,x0	// Guardamos la dirección base del framebuffer en x27
+	//---------------- CODE HERE ------------------------------------
+	bl entry_ships
+animation:
+	bl background
+
+	bl shoot
+	bl bullet_logic
+	
+	bl draw_player_ship
+	
+	bl frame_update
+	
+	bl update_bullet
+	bl update_playership
+	
+	bl delay
+
+	b animation
+//---------------------------------------------------------------
+	// Infinite Loop 
+
+InfLoop: 
+	b InfLoop
---
+++ PruebasAdolfo/logic.s
@@ -0,0 +1,113 @@
+.ifndef logic_s
+.equ logic_s,0
+.include "data.s"
+.include "graphics.s"
+
+/*
+Actualiza la posicion de la nave en el eje x
+segun una variable global para decidir si va hacia la derecha o hacia la izquierda
+esta variable tendra 3 estados:
+0: la nave se mueve hacia la derecha, hasta el que el eje x = 550
+1: la nave se mueve hacia la izquierda hasta que el eje x = 130
+2: la nave se movera hacia el eje x = 320 y se cambiara al estado 3
+3: no hay movimiento
+ */
+update_playership:
+	ldr x10,=ship_player
+	ldur w0,[x10]
+	ldur w1,[x10,4]
+	add w0,w0,1
+	stur w0,[x10]
+	stur w1,[x10,4]
+	br lr
+
+shoot:
+    ldr x17,=ship_player
+    ldr x18,=ship_enemy3
+    ldur w0,[x17] //eje x player
+    ldur w4,[x17,4]//eje y player
+    ldur w1,[x18] //eje x enemy
+    cmp x0,x1
+    B.EQ shoot_enemy1
+next_ship:
+    ldr x18,=ship_enemy4
+
+    br lr
+
+
+shoot_enemy1:
+    ldr x16,=bullet_one
+    sub x4,x4,20
+    stur w0,[x16]   
+    stur w4,[x16,4] //guardo las posiciones inciales de la bala
+    mov x3,1
+    stur w3,[x16,8] //guardo que dispare la bala
+    b next_ship
+
+
+
+explosion1:
+    ldur w3,[x11,8]
+    mov w3,1
+    stur w3,[x11,8]
+    b next_bullet
+
+bullet_logic:
+    sub sp,sp,8
+    stur lr,[sp]
+    ldr x11,=bullet_one
+    ldur w3,[x11,8]
+    cbz x3,next_bullet
+    ldur w1,[x11,4]
+    cmp w1,60
+    b.LE explosion1
+    ldur w0,[x11]
+    bl paint_bullet
+
+next_bullet:
+    
+    ldur lr,[sp]
+    add sp,sp,8
+    br lr //retrun
+
+update_bullet:
+    ldr x11,=bullet_one
+    ldur w3,[x11,8]
+    cbz x3,next_update
+    ldur w1,[x11,4]
+    sub w1,w1,1
+    stur w1,[x11,4]
+next_update:
+    br lr
+
+/*
+Funciones para manejo de animacion
+delay: genera un delay
+
+frame_update: copia todo lo dibujado en el frammebuffer secundario
+en el buffer principal, eso lo hacemos para no perder calidad en la 
+imagen al querer pintar y actualizar la imagen muy rapidamente.
+evita el 
+ */
+delay:
+        ldr x9, delay_time
+    delay_loop:
+        subs x9, x9, 1
+        b.ne delay_loop
+        br lr
+
+frame_update:
+        mov x9, SCREEN_WIDTH
+        mov x10, SCREEN_HEIGH
+    frame_loop:
+		madd x12,x10,x25,x9 
+        ldr w11, [x26,x12,lsl 2] // copio el color de cada pixel del frame secundario
+        str w11, [x27,x12,lsl 2] // lo pego en el principal
+		sub x9,x9,1
+		cbnz x9, frame_loop
+		//si x9 es 0, entonces vuelvo x9 a la derecha de la linea	
+		mov x9, SCREEN_WIDTH
+		sub x10,x10,1
+		cbnz x10,frame_loop
+        br lr // return
+.endif
